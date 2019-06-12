2f883081fe229b8c0e19f8ab5a99ea1056b8cd04
Ma <ma@qq.com>
2019-06-12 13:47:27 +0800

    第一次添加

---
+++ IdeaProjects/test_git/src/main/java/com/itheima/git/Demo.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @author Ma
+ * @version 1.0
+ * @date 2019/6/12 13:35
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
